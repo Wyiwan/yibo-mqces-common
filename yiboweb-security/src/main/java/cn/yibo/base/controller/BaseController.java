2a402ab73b0df1ee88ac608d81d599a236b0ed0a
gaoyun <2482617279@qq.com>
2019-01-15 17:13:19 +0800

    【#】代码优化;

--- yiboweb-security/src/main/java/cn/yibo/base/controller/BaseController.java
+++ yiboweb-security/src/main/java/cn/yibo/base/controller/BaseController.java
@@ -21,7 +21,8 @@
 package cn.yibo.base.controller;
 
 import cn.yibo.common.web.ServletUtils;
-import lombok.extern.java.Log;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -32,8 +33,9 @@ import javax.servlet.http.HttpServletResponse;
  *  时间: 2018-08-07
  *  版本: v1.0
  */
-@Log
 public abstract class BaseController {
+    protected Logger logger = LoggerFactory.getLogger(this.getClass());
+
     protected String SAVE_SUCCEED   = "保存成功";
 
     protected String UPDATE_SUCCEED = "修改成功";
