8e82e352ca29526668e82d06467c28e9ec90b435
gaoyun <2482617279@qq.com>
2019-01-24 18:16:36 +0800

    [+]增加Crud控制层抽象类[cn.yibo.base.controller.CrudController]，将控制层重复性的CRUD接口进行统一封装；

--- yiboweb-security/src/main/java/cn/yibo/base/controller/BaseController.java
+++ yiboweb-security/src/main/java/cn/yibo/base/controller/BaseController.java
@@ -20,12 +20,18 @@
 
 package cn.yibo.base.controller;
 
+import cn.hutool.core.map.MapUtil;
+import cn.hutool.core.util.ArrayUtil;
+import cn.hutool.core.util.StrUtil;
 import cn.yibo.common.web.ServletUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.servlet.ServletRequest;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  *  描述: 控制器抽象类
@@ -33,31 +39,44 @@ import javax.servlet.http.HttpServletResponse;
  *  时间: 2018-08-07
  *  版本: v1.0
  */
-public abstract class BaseController {
+public abstract class BaseController{
     protected Logger logger = LoggerFactory.getLogger(this.getClass());
 
     protected String SAVE_SUCCEED   = "保存成功";
 
     protected String UPDATE_SUCCEED = "修改成功";
 
-    protected String DEL_SUCCEED    = "删除成功";
+    protected String DELETE_SUCCEED = "删除成功";
 
-    protected String OPER_SUCCEED   = "操作成功";
+    protected String OPERATE_SUCCEED   = "操作成功";
 
-    protected HttpServletRequest getHttpServletRequest() {
+    protected HttpServletRequest getHttpServletRequest(){
         return ServletUtils.getRequest();
     }
 
-    protected HttpServletResponse getHttpServletResponse() {
-        return ServletUtils.getResponse();
+    /**
+     * 获得所有请求参数
+     * @return Map
+     */
+    protected Map<String, String[]> getParams(){
+        ServletRequest request = this.getHttpServletRequest();
+        if( request != null ){
+            final Map<String, String[]> map = request.getParameterMap();
+            return Collections.unmodifiableMap(map);
+        }
+        return MapUtil.newHashMap();
     }
 
-    protected String getParameter(String name) {
-        return ServletUtils.getRequest().getParameter(name);
-    }
-
-    protected void setAttribute(String name, Object value) {
-        ServletUtils.getRequest().setAttribute(name, value);
+    /**
+     * 获得所有请求参数
+     * @return Map
+     */
+    public Map<String, Object> getParamMap(){
+        Map<String, Object> params = new HashMap<String, Object>();
+        for( Map.Entry<String, String[]> entry : this.getParams().entrySet() ){
+            params.put(entry.getKey(), ArrayUtil.join(entry.getValue(), StrUtil.COMMA));
+        }
+        return params;
     }
 
 }
