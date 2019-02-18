14c8421b8fe4df8be1869736052f6e4d2969ca64
gaoyun <2482617279@qq.com>
2019-02-18 14:07:11 +0800

    【+】增加WebSocketStomp配置；

---
+++ yiboweb-boot/src/main/java/cn/yibo/boot/config/websocket/WebSocketStompConfig.java
@@ -0,0 +1,61 @@
+/*
+{*****************************************************************************
+{  广州医博-基础框架 v1.0
+{  版权信息 (c) 2018-2020 广州医博信息技术有限公司. 保留所有权利.
+{  创建人：  高云
+{  审查人：
+{  模块：消息推送模块
+{  功能描述:
+{
+{  ---------------------------------------------------------------------------
+{  维护历史:
+{  日期        维护人        维护类型
+{  ---------------------------------------------------------------------------
+{  2019-02-15  高云        新建
+{
+{  ---------------------------------------------------------------------------
+{  注：本模块代码为底层基础框架封装的boot包
+{*****************************************************************************
+*/
+
+package cn.yibo.boot.config.websocket;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+/**
+ *  描述: WebSocketStomp配置
+ *  作者：高云
+ *  邮箱: gogo163gao@163.com
+ *  时间: 2019-02-15
+ *  版本: v1.0
+ */
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketStompConfig implements WebSocketMessageBrokerConfigurer {
+    /**
+     * 注册stomp端点
+     * @param registry
+     */
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/yibo/websocket").setAllowedOrigins("*").withSockJS();
+    }
+
+    /**
+     * 配置信息代理
+     * @param registry
+     */
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        // 订阅Broker名称 user点对点 topic广播即群发
+        registry.enableSimpleBroker("/user","/topic");
+        // 全局(客户端)使用的消息前缀
+        registry.setApplicationDestinationPrefixes("/app");
+        // 点对点使用的前缀 无需配置 默认/user
+        registry.setUserDestinationPrefix("/user");
+    }
+}
